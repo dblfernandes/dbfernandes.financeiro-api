db2b2363b3e931552bc92db94bb3bc4f125a20b5
Daniel <danielbragafernandes@gmail.com>
2021-08-26 17:55:03 -0300

    adição da dependência do metamodal
    adição da dependência do metamodal

--- src/main/java/com/dbfernandes/financeiro/api/model/Pessoa.java
+++ src/main/java/com/dbfernandes/financeiro/api/model/Pessoa.java
@@ -1,5 +1,7 @@
 package com.dbfernandes.financeiro.api.model;
 
+import java.beans.Transient;
+
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "pessoa")
@@ -37,6 +41,8 @@ public class Pessoa {
 		return nome;
 	}
 
+	@JsonIgnore
+	@Transient
 	public boolean isAtivo() {
 		return ativo;
 	}
